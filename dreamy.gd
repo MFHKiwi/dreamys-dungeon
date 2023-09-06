6c4d65f04fc129c2bdb50d04a29a8e7bfb85e2aa
MFHKiwi <lauri22@online.de>
2023-09-06 13:41:16 +1200

    made lasers work

--- dreamy.gd
+++ dreamy.gd
@@ -68,6 +68,14 @@ func fire(direction):
 			bullet_instance.position += Vector2(80, 0)
 			bullet_instance.apply_impulse(Vector2(bullet_speed, 0), Vector2())
 	get_tree().get_root().call_deferred("add_child", bullet_instance)
+	var old_level = get_tree().get_root().get_node("Node2D/Starting_room")
+	get_tree().get_root().remove_child(old_level)
+	old_level.call_deferred("free")
+	var next_level_resource = load("res://room_1.tscn")
+	var next_level = next_level_resource.instantiate()
+	get_tree().get_root().add_child(next_level)
+	get_tree().get_root().content_scale_factor = 1
+
 
 func _on_hit():
 	position = Vector2(100,100)
--- laser.gd
+++ laser.gd
@@ -1,11 +1,46 @@
 extends Node2D
 
+signal hit
 
+var dreamy
+var in_area = false
+var shooting = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var timer := Timer.new()
+	add_child(timer)
+	timer.wait_time = 4
+	timer.start()
+	timer.connect("timeout", Callable(self, "_on_timeout"))
+	dreamy = get_node("../../dreamy")
+	connect("hit", Callable(dreamy, "_on_hit"))
+
+func _on_timeout():
+	$Turret.play("charge")
+	await get_tree().create_timer(2).timeout
+	shooting = true
+	$Beam.play("fire")
+	$Turret.play("shooting")
+	await get_tree().create_timer(1).timeout
+	shooting = false
+	$Beam.play("idle")
+	$Turret.play("idle")
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if shooting and in_area:
+		emit_signal("hit")
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("dreamy"):
+		in_area = true
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("dreamy"):
+		in_area = false
+	pass # Replace with function body.
